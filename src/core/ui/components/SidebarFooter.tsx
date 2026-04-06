e77f936605a90004d5a353c02c087b339fedd9fd
Steven Harding <sharding1012@gmail.com>
2026-04-05 20:22:53 -0700

    add top border to sidebar footer

--- src/core/ui/components/SidebarFooter.tsx
+++ src/core/ui/components/SidebarFooter.tsx
@@ -1,6 +1,6 @@
 export default function SidebarFooter() {
   return (
-    <div className="mt-auto">
+    <div className="mt-auto border-t-[0.5px] border-gray-200">
       <p>Sarah Chen</p>
       <p>Hotel Owner</p>
     </div>
